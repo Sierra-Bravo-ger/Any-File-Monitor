dcc643f598886e7ba239cc02e9b8a1dfb968e2c7
Sierra-Bravo-ger <sebastian.bride@gmail.com>
2025-04-05 17:42:48 +0200

    new file:   .gitignore 	new file:   AFMlog/AnyFileMonitor.ps1 	new file:   AFMlog/config.ini 	modified:   ARCHIV/ERROR/test2.error 	deleted:    ARCHIV/ERROR/test2.hl7 	modified:   ARCHIV/ERROR/test3.error 	deleted:    ARCHIV/ERROR/test3.hl7 	modified:   ARCHIV/ERROR/test4.error 	deleted:    ARCHIV/ERROR/test4.hl7 	modified:   ARCHIV/ERROR/test5.error 	deleted:    ARCHIV/ERROR/test5.hl7 	modified:   ARCHIV/ERROR/test6.error 	deleted:    ARCHIV/ERROR/test6.hl7 	modified:   ARCHIV/ERROR/test7.error 	deleted:    ARCHIV/ERROR/test7.hl7 	modified:   ARCHIV/ERROR/test8.error 	deleted:    ARCHIV/ERROR/test8.hl7 	modified:   ARCHIV/ERROR/test9.error 	deleted:    ARCHIV/ERROR/test9.hl7 	deleted:    ARCHIV/test2.hl7 	deleted:    ARCHIV/test3.hl7 	deleted:    ARCHIV/test4.hl7 	deleted:    ARCHIV/test5.hl7 	deleted:    ARCHIV/test6.hl7 	deleted:    ARCHIV/test7.hl7 	deleted:    ARCHIV/test8.hl7 	deleted:    ARCHIV/test9.hl7 	deleted:    INPUT/test2.hl7 	deleted:    INPUT/test3.hl7 	deleted:    INPUT/test4.hl7 	deleted:    INPUT/test5.hl7 	deleted:    INPUT/test6.hl7 	deleted:    INPUT/test7.hl7 	deleted:    INPUT/test8.hl7 	deleted:    INPUT/test9.hl7

---
+++ AFMlog/AnyFileMonitor.ps1
@@ -0,0 +1,96 @@
+# AnyFileMonitor (AFM) Hauptskript
+
+# Konfig einlesen
+$configPath = Join-Path $PSScriptRoot 'config.ini'
+$config = @{}
+Get-Content $configPath | Where-Object { $_ -match '=' } | ForEach-Object {
+    $parts = $_ -split '=', 2
+    $config[$parts[0].Trim()] = $parts[1].Trim()
+}
+
+# Pfade aus Konfig
+$inputPath = $config['inputPath']
+$archivPath = $config['archivPath']
+$errorPath = $config['errorPath']
+
+# Logs aus Konfig
+$statusLog = $config['AFMstatusLog']
+$errorLog = $config['AFMerrorLog']
+$seenList = $config['AFMseenList']
+$inputDetailLog = $config['AFMinputDetailLog']
+
+# Erweiterungen aus Konfig
+$fileExtensions = @('.hl7', '.txt')  # Standardwerte falls nicht in Konfig definiert
+if ($config.ContainsKey('fileExtensions')) {
+    $fileExtensions = ($config['fileExtensions'] -split ',') | ForEach-Object { $_.Trim().ToLower() }
+}
+
+# Sicherstellen, dass Logdateien existieren
+if (!(Test-Path $statusLog)) {
+    "Zeitpunkt;Input;Archiv;Error" | Out-File -FilePath $statusLog -Encoding UTF8
+}
+if (!(Test-Path $errorLog)) {
+    "Zeitpunkt;ErrorDatei;Fehlermeldung" | Out-File -FilePath $errorLog -Encoding UTF8
+}
+if (!(Test-Path $seenList)) {
+    New-Item $seenList -ItemType File -Force | Out-Null
+}
+if (!(Test-Path $inputDetailLog)) {
+    "Zeitpunkt;Dateiname;Anzahl;Dateien" | Out-File -FilePath $inputDetailLog -Encoding UTF8
+}
+
+# Vorher gesehene Dateien laden (für Error-Verarbeitung)
+$seen = @()
+if (Test-Path $seenList) {
+    $seen = Get-Content $seenList
+}
+
+# Zeitstempel für Logeintrag
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+# INPUT-Ordner: Dateien zählen ohne zu modifizieren
+$inputFiles = Get-ChildItem -Path $inputPath -File -ErrorAction SilentlyContinue | 
+              Where-Object { $fileExtensions -contains $_.Extension.ToLower() }
+$inputCount = $inputFiles.Count
+$inputFileNames = ($inputFiles | Select-Object -ExpandProperty Name) -join ','
+
+# ARCHIV-Ordner: Dateien zählen
+$archivFiles = Get-ChildItem -Path $archivPath -File -ErrorAction SilentlyContinue
+$archivCount = $archivFiles.Count
+
+# ERROR-Ordner: Dateien zählen
+$errorFiles = Get-ChildItem -Path $errorPath -File -ErrorAction SilentlyContinue
+$errorCount = $errorFiles.Count
+
+# Status-Log aktualisieren
+"$timestamp;$inputCount;$archivCount;$errorCount" | Out-File -Append -FilePath $statusLog -Encoding UTF8
+
+# Input-Detail-Log aktualisieren
+"$timestamp;$inputCount;$inputFileNames" | Out-File -Append -FilePath $inputDetailLog -Encoding UTF8
+
+# Nachträgliche Fehlerverarbeitung (nur für Dateien, die noch nicht gesehen wurden und älter als 1 Minute sind)
+$errorFilesForProcessing = Get-ChildItem -Path $errorPath -Filter "*.error" -File -ErrorAction SilentlyContinue | 
+                           Where-Object { $_.LastWriteTime -lt (Get-Date).AddMinutes(-1) }
+foreach ($errFile in $errorFilesForProcessing) {
+    if ($seen -contains $errFile.Name) { continue }
+    
+    try {
+        $errorFilename = $errFile.Name
+        
+        # .error-Datei lesen
+        $errorText = Get-Content $errFile.FullName -ErrorAction Stop | Out-String
+        $errorText = $errorText.Trim().Replace("`r`n", " ")
+        if ($errorText.Length -gt 300) { $errorText = $errorText.Substring(0,300) }
+
+        # Fehlerlog schreiben
+        "$timestamp;$errorFilename;$errorText" | Out-File -FilePath $errorLog -Append -Encoding UTF8
+
+        # Gesehen-Liste aktualisieren
+        Add-Content $seenList $errorFilename
+    }
+    catch {
+        Write-Host "Warnung: Fehler beim Verarbeiten von $($errFile.Name) - $_"
+    }
+}
+
+Write-Host "AFM abgeschlossen."
